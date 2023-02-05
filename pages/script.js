8b10a89ca251584f110526327a731843b3e266ec
sbravindran03 <114281707+sbravindran03@users.noreply.github.com>
2023-02-05 20:59:41 +0530

    Delete pages directory

--- pages/script.js
+++
@@ -1,5 +0,0 @@
-const open = document.getElementById("open");
-const close = document.getElementById("close");
-const container = document.querySelector(".container");
-open.addEventListner('click', () => container.class.add('show-nav'));
-close.addEventListner('click', () => container.class.add('show-nav'));
